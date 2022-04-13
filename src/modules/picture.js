60b0bda4b99dccc9bf1f817d037ec16958671bb0
buhariaminat <buhariaminat@yahoo.com>
2022-04-13 18:19:24 +0100

    Make change to display.js

--- src/modules/picture.js
+++ src/modules/picture.js
@@ -1,8 +1,9 @@
-import image from './assets/images/pancake.jpg';
+import image from '../assets/images/pancake.jpg';
 
 const imageContainer = document.querySelector('.container-img');
 
 // /////////////////// Create dom elements
+
 const createElement = (element = '', className = [], text = '', src = '', alt = '') => {
   const imageElement = document.createElement(element);
   imageElement.classList.add(...className);
@@ -12,9 +13,9 @@ const createElement = (element = '', className = [], text = '', src = '', alt = 
   return imageElement;
 };
 
-const displayImage = () => {
+const displayPopup = () => {
   const img = createElement('img', ['pop-img'], '', image, 'pancake');
   imageContainer.appendChild(img);
 };
 
-export { displayImage as default };
+export { displayPopup as default };
