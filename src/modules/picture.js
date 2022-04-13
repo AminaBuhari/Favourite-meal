3ef13306d26b3c90c170bc9b9bc876b145df2978
buhariaminat <buhariaminat@yahoo.com>
2022-04-14 00:47:35 +0100

    Update popup

--- src/modules/picture.js
+++ src/modules/picture.js
@@ -1,97 +1,38 @@
 import image from '../assets/images/pancake.jpg';
 
-const popContainer = document.querySelector('.comment-popup');
-
+const popHeader = document.querySelector('.pop-header');
+const iconClose = document.querySelector('.icon-close');
+const main = document.querySelector('.pop-main');
 // /////////////////// Create dom elements
 
-const createElement = (
-  element = '',
-  className = [],
-  text = '',
-  src = '',
-  alt = '',
-  href = '',
-  type = '',
-  name = '',
-  id = '',
-  placeholder = '',
-  cols = '',
-  rows = '',
-) => {
-  const elements = document.createElement(element);
+const createElement = (name = '', className = [], text = '', src = '', alt = '') => {
+  const elements = document.createElement(name);
   elements.classList.add(...className);
   elements.textContent = text;
   elements.src = src;
   elements.alt = alt;
-  elements.href = href;
-  elements.type = type;
-  elements.name = name;
-  elements.id = id;
-  elements.placeholder = placeholder;
-  elements.cols = cols;
-  elements.rows = rows;
-
   return elements;
 };
 
 const displayPopup = () => {
-  const containerImage = createElement('div', ['container-img']);
-  const img = createElement('img', ['pop-img'], '', image, 'pancake');
-  const anchor = createElement('a', '', '', '', '', '#');
-  const icons = createElement('i', ['fa-solid', 'fa-xmark', 'fa-3x']);
-  const firstHeader = createElement('h2', '', 'Menu 3');
-  const aboutContainer = createElement(' div', ['about']);
-  const descriptionCont = createElement('div', ['description']);
-  const paragraphOne = createElement('p', '', 'Food: pancake');
-  const paragraphTwo = createElement('p', '', 'Origin: America');
-  const desCont = createElement('div', ['des']);
-  const paragraphThree = createElement('p', '', 'Calories: 200kcal');
-  const paragraphFour = createElement('p', '', 'Toppings: Chocolate Syrup');
-  const secondHeader = createElement('h5', '', 'Comments(2)');
-  const paragraphFive = createElement(
-    'p',
-    'date',
-    '03/04/2022 Priya: The perfect morning breakfast!',
-  );
-  const paragraphSix = createElement('p', 'date', '09/04/2022 Alex: I love');
-  const thirdHeader = createElement('h5', '', 'Add a comment');
-  const form = createElement('form');
-  const input = createElement(
-    'input',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'text',
-    'name',
-    'name',
-    'Your name',
-  );
-  const textarea = createElement(
-    'textarea',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'textarea',
-    'textarea',
-    'Your insights',
-    '30',
-    '5',
-  );
-  const button = createElement('button', '', 'comment');
+  const img = createElement('img', ['pop-img'], '', image, 'Picture');
+  const foodname = createElement('h2', ['food-name'], 'Menu 3');
+  const comment = createElement('h2', ['comment'], 'Comments');
+  const numComments = createElement('span');
+  const aboutCont = createElement('div', ['about']);
+  const desc1 = createElement('div', ['desc1']);
+  const desc2 = createElement('div', ['desc2']);
+  const p1 = createElement('p', '', 'Food: pancake');
+  const p2 = createElement('p', '', 'Origin: America');
+  const p3 = createElement('p', '', 'Calories: 200kcal');
+  const p4 = createElement('p', '', 'Toppings: Chocolate syrup');
 
-  containerImage.appendChild(img, anchor);
-  anchor.appendChild(icons);
-  descriptionCont.appendChild(paragraphOne, paragraphTwo);
-  desCont.appendChild(paragraphThree, paragraphFour);
-  aboutContainer.appendChild(descriptionCont, desCont);
-  form.appendChild(input, textarea, button);
-
-  popContainer.append(paragraphOne);
+  popHeader.insertBefore(img, iconClose);
+  desc1.append(p1, p2);
+  desc2.append(p3, p4);
+  aboutCont.append(desc1, desc2);
+  comment.append(numComments);
+  main.append(foodname, aboutCont, comment);
 };
 
-export { displayPopup as default };
+export default displayPopup;
