cbd762d5bef5e01445b93b5348dc9de3ae6c3926
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-14 22:35:08 +0300

    Modify util.js file

--- src/test/util.test.js
+++ src/test/util.test.js
@@ -1,13 +1,13 @@
-const { displayCount, displayFood } = require('../modules/util.js');
+const util = require('../modules/util.js');
 
 jest.mock('../modules/util');
 
 describe('Count Favoirte Foods', () => {
   test('should take the number of items and return to the dom', () => {
-    expect(displayCount(5)).toBe(5);
+    expect(util.displayCount(5)).toBe(5);
   });
 
   test('should pass through all of the items and increase the value by 1', () => {
-    expect(displayFood()).toBe(7);
+    expect(util.displayFood()).toBe(7);
   });
 });
