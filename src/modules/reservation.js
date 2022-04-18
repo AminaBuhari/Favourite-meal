4f7114d2833c23fae61564963010f50056866c76
buhariaminat <buhariaminat@yahoo.com>
2022-04-18 20:56:01 +0100

    Update the reservation api

--- src/modules/reservation.js
+++ src/modules/reservation.js
@@ -1,7 +1,6 @@
 import {
-  reservations, getReservations, postReservations
-} from './config';
-
+  reservations, getReservations, postReservations,
+} from './config.js';
 
 const container = document.querySelector('.container');
 const pop = document.querySelector('.popup-reservation');
@@ -19,23 +18,23 @@ const closePopup = (btn) => {
 
 // /////////////////// Create dom elements
 const createElement = (
-    element = '',
-    className = [],
-    text = '',
-    src = '',
-    alt = '',
-  ) => {
-    const newElement = document.createElement(element);
-    newElement.classList.add(...className);
-    newElement.textContent = text;
-    newElement.src = src;
-    newElement.alt = alt;
-    return newElement;
-  };
+  element = '',
+  className = [],
+  text = '',
+  src = '',
+  alt = '',
+) => {
+  const newElement = document.createElement(element);
+  newElement.classList.add(...className);
+  newElement.textContent = text;
+  newElement.src = src;
+  newElement.alt = alt;
+  return newElement;
+};
 
 const countComment = (data) => data.length;
 
-const addReservations = (foodname, aboutCont, comment, commentGroup, numComments) => {
+const addReservations = (e, foodname, aboutCont, comment, commentGroup, numComments) => {
   form.addEventListener('submit', (event) => {
         event.preventDefault();
         const name = form.elements.name.value;
@@ -44,12 +43,12 @@ const addReservations = (foodname, aboutCont, comment, commentGroup, numComments
         reservations.username = name;
         reservations.date_start = dateStart;
         reservations.date_end = dateEnd;
+        reservations.item_id = e.idCategory;
         postReservations(reservations);
-        const user = createElement('p', ['user'], `${name}`);
         const startDate = createElement('p', ['comm'], `${dateStart}-`);
         const endDate = createElement('p', ['comm'], `${dateEnd}`);
+        const user = createElement('p', ['user'], `${name}`);
         const commrap = createElement('div', ['commrap']);
-
         commrap.append(user, startDate, endDate);
         commentGroup.appendChild(commrap);
         numComments.textContent = Number(numComments.textContent) + 1;
@@ -62,45 +61,43 @@ const addReservations = (foodname, aboutCont, comment, commentGroup, numComments
 
 const displayReserve = async (btn, e) => {
   btn.addEventListener('click', async () => {
-    container.classList.add('hide');
-    pop.classList.remove('hide');
-    pop.classList.add('show');
+        container.classList.add('hide');
+        pop.classList.remove('hide');
+        pop.classList.add('show');
+        main.innerHTML = '';
+        imgCont.innerHTML = '';
+        const imag = createElement('img', ['pop-img'], '', e.strCategoryThumb, 'Picture');
+        const data = await getReservations(e.idCategory);
+        const numberOfReservations = countComment(data);
+        const commentGroup = createElement('div', ['comment-group']);
+        const foodname = createElement('h2', ['food-name'], e.strCategory);
+        const comment = createElement('h2', ['comment'], 'Reservations');
+        const aboutCont = createElement('div', ['about']);
+        const desc1 = createElement('div', ['desc1']);
+        const desc2 = createElement('div', ['desc2']);
+        const p1 = createElement('p', '', `Food: ${e.strCategory}`);
+        const p2 = createElement('p', '', 'Origin: America');
+        const p3 = createElement('p', '', 'Calories: 200kcal');
+        const p4 = createElement('p', '', 'Toppings: Chocolate syrup');
+        const numComments = createElement('span', ['comment-counter'], numberOfReservations);
+        imgCont.append(imag);
+        desc1.append(p1, p2);
+        desc2.append(p3, p4);
+        aboutCont.append(desc1, desc2);
+        comment.append(numComments);
+        closePopup(close);
+        data.forEach((ele) => {
+            const commrap = createElement('div', ['commrap']);
+            const comm = createElement('p', ['comm'], ele.username);
+            const date = createElement('p', ['date'], ele.date_start);
+            const dateend = createElement('p', ['date'], ele.date_end);
+            commrap.append(comm, date, dateend);
+            commentGroup.append(commrap);
+        });
 
-    main.innerHTML = '';
-    imgCont.innerHTML = '';
-    const imag = createElement('img', ['pop-img'], '', e.strCategoryThumb, 'Picture');
-    const data = await getReservations(e.idCategory);
-    const numberOfReservations = countComment(data);
-    const commentGroup = createElement('div', ['comment-group']);
-    const foodname = createElement('h2', ['food-name'], e.strCategory);
-    const comment = createElement('h2', ['comment'], 'Reservations');
-    const aboutCont = createElement('div', ['about']);
-    const desc1 = createElement('div', ['desc1']);
-    const desc2 = createElement('div', ['desc2']);
-    const p1 = createElement('p', '', `Food: ${e.strCategory}`);
-    const p2 = createElement('p', '', 'Origin: America');
-    const p3 = createElement('p', '', 'Calories: 200kcal');
-    const p4 = createElement('p', '', 'Toppings: Chocolate syrup');
-    const numComments = createElement('span', ['comment-counter'], numberOfReservations);
-
-    imgCont.append(imag);
-    desc1.append(p1, p2);
-    desc2.append(p3, p4);
-    aboutCont.append(desc1, desc2);
-    comment.append(numComments);
-    closePopup(close);
-    data.forEach((ele) => {
-      const commrap = createElement('div', ['commrap']);
-      const comm = createElement('p', ['comm'], ele.username);
-      const date = createElement('p', ['date'], ele.date_start);
-      const dateend = createElement('p', ['date'], ele.date_end);
-      commrap.append(comm, date, dateend);
-      commentGroup.append(commrap);
+        main.append(foodname, aboutCont, comment, commentGroup);
+        addReservations(e, foodname, aboutCont, comment, commentGroup, numComments);
     });
-
-    main.append(foodname, aboutCont, comment, commentGroup);
-    addReservations(foodname, aboutCont, comment, commentGroup, numComments);
-  });
 };
 
 export { displayReserve as default };
